9be07c1ef26dae8a44df376fe99f0dfc15ae8393
BorysKravtsov <mamkinboris3@gmail.com>
2024-05-02 18:10:43 +0200

    responsive updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-$(function(){
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav');
 
+    hamburger.addEventListener('click', function() {
+        nav.classList.toggle('active');
+    });
 });
+
+const swiper = new Swiper('.swiper', {
+  effect: 'flip',
+  flipEffect: {
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+    a11y: {
+      prevSlideMessage: 'Previous slide',
+      nextSlideMessage: 'Next slide',
+    },
+  });
+  
