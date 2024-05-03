4c3655329e1720fed1a2d5f76157de10e1aee0e7
BorysKravtsov <mamkinboris3@gmail.com>
2024-05-03 19:39:56 +0200

    responsive fixes commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
     hamburger.addEventListener('click', function() {
         nav.classList.toggle('active');
+        if (hamburger.innerHTML === '☰') {
+            hamburger.innerHTML = '✕'; 
+        } else {
+            hamburger.innerHTML = '☰'; 
+        }
     });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const header = document.querySelector('.header__navbar');
+  const headerOffset = header.offsetTop; 
+
+  function scrollHandler() {
+      if (window.pageYOffset > headerOffset) {
+          header.classList.add('fixed-header');
+      } else {
+          header.classList.remove('fixed-header');
+      }
+  }
+
+  window.addEventListener('scroll', scrollHandler);
 });
 
 const swiper = new Swiper('.swiper', {
@@ -16,6 +36,28 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+    a11y: {
+      prevSlideMessage: 'Previous slide',
+      nextSlideMessage: 'Next slide',
+    },
+    autoplay: {
+      delay: 3000, 
+      disableOnInteraction: false, 
+    },
+  });
+  const swiper3 = new Swiper('.swiper3', {
+    effect: 'flip',
+    flipEffect: {
+      slideShadows: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 1500, 
+      disableOnInteraction: false, 
+    },
     a11y: {
       prevSlideMessage: 'Previous slide',
       nextSlideMessage: 'Next slide',
