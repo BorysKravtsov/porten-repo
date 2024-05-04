847fd0198a664981797444f98b72fbaa35d04755
BorysKravtsov <mamkinboris3@gmail.com>
2024-05-04 23:29:28 +0200

    bug fix 1.0

--- js/main.js
+++ js/main.js
@@ -1,33 +1,81 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.nav');
-
-    hamburger.addEventListener('click', function() {
-        nav.classList.toggle('active');
-        if (hamburger.innerHTML === '☰') {
-            hamburger.innerHTML = '✕'; 
-        } else {
-            hamburger.innerHTML = '☰'; 
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  const header = document.querySelector('.header__navbar');
-  const headerOffset = header.offsetTop; 
-
-  function scrollHandler() {
-      if (window.pageYOffset > headerOffset) {
-          header.classList.add('fixed-header');
-      } else {
-          header.classList.remove('fixed-header');
-      }
+function toggleNav(hamburger, nav, body) {
+  if (nav.classList.contains('active')) {
+    nav.style.animation = 'burger-menu-hide 0.7s forwards';
+    nav.addEventListener('animationend', () => {
+      nav.classList.remove('active');
+      body.style.overflow = 'auto';
+    }, { once: true });
+  } else {
+    nav.classList.add('active');
+    nav.style.animation = 'burger-menu-show 0.7s forwards';
+    body.style.overflow = 'hidden';
   }
+  hamburger.classList.toggle('rotated');
+  hamburger.innerHTML = hamburger.innerHTML === '☰' ? '✕' : '☰';
+}
 
-  window.addEventListener('scroll', scrollHandler);
+function handleHeaderScroll(header, mainContent) {
+  const headerOffset = header.offsetTop;
+  window.addEventListener('scroll', function () {
+    let currentScrollPosition = window.pageYOffset;
+    if (currentScrollPosition > headerOffset) {
+      header.classList.add('fixed-header');
+      mainContent.style.paddingTop = header.offsetHeight + 'px';
+    } else {
+      header.classList.remove('fixed-header');
+      mainContent.style.paddingTop = '0px';
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.nav');
+  const body = document.body;
+  const header = document.querySelector('.header__navbar');
+  const mainContent = document.querySelector('.main');
+
+  hamburger.addEventListener('click', function () {
+    toggleNav(hamburger, nav, body);
+  });
+
+  handleHeaderScroll(header, mainContent);
 });
+
+const container = document.querySelector('.header__navbar');
+const element = document.querySelector('.nav.active');
+
+
+const containerWidth = container.offsetWidth;
+
+
+element.style.width = containerWidth + 'px';
+
+
+
+
+
 
 const swiper = new Swiper('.swiper', {
+  effect: 'flip',
+  flipEffect: {
+    slideShadows: false,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  a11y: {
+    prevSlideMessage: 'Previous slide',
+    nextSlideMessage: 'Next slide',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+const swiper3 = new Swiper('.swiper3', {
   effect: 'flip',
   flipEffect: {
     slideShadows: false,
@@ -36,31 +84,13 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-    a11y: {
-      prevSlideMessage: 'Previous slide',
-      nextSlideMessage: 'Next slide',
-    },
-    autoplay: {
-      delay: 3000, 
-      disableOnInteraction: false, 
-    },
-  });
-  const swiper3 = new Swiper('.swiper3', {
-    effect: 'flip',
-    flipEffect: {
-      slideShadows: false,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-      delay: 1500, 
-      disableOnInteraction: false, 
-    },
-    a11y: {
-      prevSlideMessage: 'Previous slide',
-      nextSlideMessage: 'Next slide',
-    },
-  });
-  
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  a11y: {
+    prevSlideMessage: 'Previous slide',
+    nextSlideMessage: 'Next slide',
+  },
+});
+
